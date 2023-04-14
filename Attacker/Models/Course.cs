c948e242f044abd7c33d3f0d364957d7d0da5cd3
Gulshan <gulshankumar.mailid01@gmail.com>
2023-04-14 18:36:27 +0530

    Add project files.

---
+++ Attacker/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Attacker.Models
+{
+    public class Course
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Code { get; set; } = "";
+        public ICollection<StudentCourse>? StudentCourses { get; set; } = new HashSet<StudentCourse>();
+    }
+}
